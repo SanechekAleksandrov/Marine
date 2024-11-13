00b776c50e99a37b433d2bed9ae70145771d467d
Sanechek Aleksandrov <sanechek-1994@mail.ru>
2024-11-13 06:07:10 +0100

    1.2

--- start/js/main.js
+++ start/js/main.js
@@ -1,3 +1,14 @@
 $(function(){
 
+$('.header-slider').slick({
+  dots: true, 
+  prevArrow: '<button type="button" class="slick-next">  <img src="images/next.svg" alt="O"></button>',
+  nextArrow: '<button type="button" class="slick-prev">  <img src="images/prev.svg" alt="O"></button>',
+}); 
+
+
+
+
+
+
 });
